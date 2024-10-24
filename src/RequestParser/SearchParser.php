4ca52c3b07646b900db2eecb40c8b5b0bff09028
tanmai <tanmai@tanmais-Mac-mini.local>
2024-10-24 08:55:11 +0700

    fix: search

--- src/RequestParser/SearchParser.php
+++ src/RequestParser/SearchParser.php
@@ -7,37 +7,45 @@ use Illuminate\Support\Str;
 class SearchParser
 {
     /**
-     * Search parser
+     * Parse the search query.
      *
      * @param  array  $queryString
-     * @param  array<string>  $filterable
-     * @return array
+     * @param  array<{column: array<string>, value: string}> $filterable
+     * @return array<{column: array<string>, value: string}>
      */
     public function parse(array $queryString, array $filterable): array
     {
-        if (!$queryString) {
-            return [
-                'column' => '',
-                'value' => '',
-            ];
-        }
-
-        $column = Str::of($queryString['column'] ?? '')
+        // Extract the column from the query string and filter it
+        $columns = Str::of($queryString['column'] ?? '')
             ->trim(',')
             ->explode(',')
             ->filter(function ($value) use ($filterable) {
                 $field = explode('.', $value)[0];
+                if (!$field) {
+                    return false;
+                }
+
                 if ($filterable) {
                     return in_array($field, $filterable, true);
                 }
 
                 return true;
             })
-            ->map(fn ($value) => trim($value))
+            ->map(fn($value) => trim($value))
+            ->values()
             ->all();
 
+        // Return empty column and value if no valid columns are found
+        if (!$columns) {
+            return [
+                'column' => '',
+                'value' => '',
+            ];
+        }
+
+        // Return the parsed column and value
         return [
-            'column' => $column,
+            'column' => $columns,
             'value' => $queryString['value'] ?? '',
         ];
     }
